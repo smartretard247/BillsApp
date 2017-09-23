ea8a7ef46b1eb5967c33c6bc4af89734d1e67b6e
smartretard247 <smartretard247@users.noreply.github.com>
2017-09-23 09:33:01 +0900

    Initial Commit

---
+++ Bills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bills
+//
+//  Created by Jeezy on 9/23/17.
+//  Copyright © 2017 Simply Silver AKY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
